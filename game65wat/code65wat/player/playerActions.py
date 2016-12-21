df9d65c270c07ba28e412fef5e4f2fde72c3f4c4
arjenp <arjenplantinga@hotmail.com>
2016-12-21 13:22:44 +0100

    Made a beginning with the skeleton. With the information for the player and the playeractions.

---
+++ game65wat/code65wat/player/playerActions.py
@@ -0,0 +1,34 @@
+class PlayerActions:
+
+    #the general classs where the players actions get defined.
+
+
+    def attack(infoPlayer):
+        # make a function that lets the player attack an other player. The info is the information about the attacked player
+        print ("attacking")
+
+    def defence(infoPlayer):
+        #Make a function that lets the player defend from and other player. The info is the information about the attacking player
+        #If players cant defend then show an notification to the player where he sees he is attacked and can see the results
+        print ("Defending")
+
+    def scout(infoPlayer):
+        #Scout another player where you can see the stats of that player. The info is the information for the to be scouted player
+        # retun information is succesvol and fail message when not succesful
+        print("scouting")
+
+    def upgrade(infoResources):
+        # Upgrade a resource. The info is the resource to be upgraded
+        print("Upgrading")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ game65wat/code65wat/player/playerInfo.py
@@ -0,0 +1,9 @@
+class PlayerInfo:
+
+
+    def getCurrentPlayerInfo(playerInfo):
+        #get the current information About the player to show to the player
+        playerResources = []
+
+
+
