4a724df3d2d2cb0dfd5acf6f2ee1a5872299b98d
Jeroen Stravers <jeroen.stravers@live.nl>
2016-12-22 23:46:16 +0100

    both unit tests pass

--- game65wat/code65wat/tests.py
+++ game65wat/code65wat/tests.py
@@ -1,15 +1,21 @@
 from django.test import TestCase
-from .models import RegisteredUser
+from code65wat.models import RegisteredUser
 
 
 class RegisteredUserTestCase(TestCase):
+
     def setUp(self):
-        TestCase.objects.create(
+        RegisteredUser.objects.create(
             username="Patrick",
             password="vliegerszijntegek"
         )
 
     def test_user_created(self):
         """a user is correctly stored and retrievable by name (PK)"""
-        retrieved_user = RegisteredUser.objects.get(username="lion")
+        retrieved_user = RegisteredUser.objects.get(username="Patrick")
         self.assertEqual(retrieved_user.password, 'vliegerszijntegek')
+
+    def test_user_has_resource(self):
+        """a user is created with the default value of resources"""
+        retrieved_user = RegisteredUser.objects.get(username="Patrick")
+        self.assertEqual(retrieved_user.resource_x, 100)
